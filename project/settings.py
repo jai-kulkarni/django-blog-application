28f95198c05feb96bf7fb01250adbd438d60699b
Jai Kulkarni <jaikulkarni5@gmail.com>
2024-11-03 12:06:27 +0530

    Pagination & Reset Password

--- project/settings.py
+++ project/settings.py
@@ -134,3 +134,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('DJ_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('DJ_PASS')
+
+DEFAULT_FROM_EMAIL = f'Django Blog {EMAIL_HOST_USER}'
